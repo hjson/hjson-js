611b6530921d96effc3320b036346f6b1359f421
Christian Zangl <coralllama@gmail.com>
2014-04-08 18:13:38 +0200

    wrap node export

--- sample/sample.js
+++ sample/sample.js
@@ -3,8 +3,12 @@ var Hjson = require("hjson");
 var fs=require("fs");
 var text = fs.readFileSync("readme.hjson", "utf8");
 
+// parse either JSON or Hjson
 var data = Hjson.parse(text);
 console.log(data.hello);
 
-// convert to json
+// convert to JSON
 console.log(JSON.stringify(data));
+
+// convert to Hjson
+console.log(Hjson.stringify(data));
