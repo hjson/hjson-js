f659c5901fdcdd5f794b7bf068aeae4144a3e818
Christian Zangl <coralllama@gmail.com>
2014-05-09 11:50:17 +0200

    chg sample

--- sample/sample.js
+++ sample/sample.js
@@ -6,9 +6,13 @@ var text = fs.readFileSync("readme.hjson", "utf8");
 // parse either JSON or Hjson
 var data = Hjson.parse(text);
 console.log(data.hello);
+console.log();
 
 // convert to JSON
+console.log("JSON output:");
 console.log(JSON.stringify(data));
+console.log();
 
 // convert to Hjson
+console.log("Hjson output:");
 console.log(Hjson.stringify(data));
