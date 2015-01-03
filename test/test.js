2b1a6896533ab3ce34736fa99ea45e579c96dc99
Christian Zangl <coralllama@gmail.com>
2015-01-03 22:04:18 +0100

    chg style

--- test/test.js
+++ test/test.js
@@ -6,8 +6,7 @@ var rootDir = path.normalize(path.join(__dirname, "asset"));
 
 Hjson.setEndOfLine("\n");
 
-function showErr(cap, s1, s2)
-{
+function showErr(cap, s1, s2) {
   console.log(cap+" FAILED!");
   console.log("--- actual:");
   console.log(s1);
@@ -16,8 +15,7 @@ function showErr(cap, s1, s2)
   process.exit(1);
 }
 
-fs.readdirSync(rootDir).forEach(function(file)
-{
+fs.readdirSync(rootDir).forEach(function(file) {
   var name = file.split("_test.hjson");
   if (name.length !== 2) return;
   name = name[0];
@@ -25,8 +23,7 @@ fs.readdirSync(rootDir).forEach(function(file)
   var text = fs.readFileSync(path.join(rootDir, file), "utf8");
   var result = JSON.parse(fs.readFileSync(path.join(rootDir, name+"_result.json"), "utf8"));
 
-  try
-  {
+  try {
     var data = Hjson.parse(text);
     var data1 = JSON.stringify(data, null, 2), data2 = JSON.stringify(result.data, null, 2);
     var hjson1 = Hjson.stringify(data);
@@ -36,8 +33,7 @@ fs.readdirSync(rootDir).forEach(function(file)
     else if (hjson1 !== hjson2) showErr(name+" stringify", hjson1, hjson2);
     else console.log(name+" SUCCESS");
   }
-  catch (err)
-  {
+  catch (err) {
     if (result.err) console.log(name+" SUCCESS");
     else showErr(name+" exception", err.toString(), "");
   }
